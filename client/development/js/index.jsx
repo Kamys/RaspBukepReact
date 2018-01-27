c951481df709eae8b032688d5127087699d8f0b5
Kamys <nikita.kamyshenko@mail.ru>
2018-01-27 22:36:09 +0300

    Add bootstrap

--- client/development/js/index.jsx
+++ client/development/js/index.jsx
@@ -4,7 +4,16 @@ import ReactDOM from "react-dom";
 
 class HelloWorld extends React.Component {
     render() {
-        return <h1>HelloWorld</h1>;
+        return (<div>
+            <button type="button" className="btn btn-primary">Главный</button>
+            <button type="button" className="btn btn-secondary">Вторичный</button>
+            <button type="button" className="btn btn-success">Успех</button>
+            <button type="button" className="btn btn-danger">Опасность</button>
+            <button type="button" className="btn btn-warning">Предупреждение</button>
+            <button type="button" className="btn btn-info">Инфо</button>
+            <button type="button" className="btn btn-light">Светлый</button>
+            <button type="button" className="btn btn-dark">Темный</button>
+        </div>);
     }
 }
 
